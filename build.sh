b54f1f55f7f4babfac99f579c4986fb5426f4ec6
arkarsg <alxarkar@gmail.com>
2024-04-08 21:41:04 +0800

    Add profiles, add shell script

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+helm upgrade --cleanup-on-fail --install jupyterhub-3.3.6 jupyterhub/jupyterhub \
+  --namespace jhub \
+  --create-namespace \
+  --values config.yaml \
+  --values profiles.yaml \
+  --timeout=10m
